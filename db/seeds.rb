53b186b61ee1adbf3a6436d340b2d1f499a31139
Fabrizio027 <fabrizio.27.02.98@gmail.com>
2022-02-09 16:25:32 -0500

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Piero", password: "password")
+User.create(username: "Richard", password: "password")
+User.create(username: "Carmen", password: "password")
+User.create(username: "Axel", password: "password")
+User.create(username: "Cristian", password: "password")
